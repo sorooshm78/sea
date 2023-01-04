7e373f27a3a65bc6065d260f8dd84311a7c65d55
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-04 15:42:59 +0330

    create empty table and show it

---
+++ sea/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.single_player_view, name="single_player"),
+    path('<int:button>', views.select, name="select"),
+]
--- sea/views.py
+++ sea/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+ROW = 6
+COLUMN = 4
+
+def make_table_game(row, column):
+    table = [0] * row * column
+    return table
+
+
+def single_player_view(request):
+    table = make_table_game(ROW, COLUMN)
+    
+    context = {
+        'table' : table,
+        'row' : ROW,
+        'column' : COLUMN,
+    } 
+
+    return render(request, "sea/single_player.html", context=context)
+
+
+def select(request, butten):
+    return HttpResponse('ok recive selected')
