458f2902d85b558ec1cd1e1299fa5c4d0c995231
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-07 11:39:37 +0330

    get out logic from view

--- sea/views.py
+++ sea/views.py
@@ -1,32 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# TODO: add this const to settings.py
-ROW = 10
-COLUMN = 10
-
-class SeaBattle:
-    def __init__(self, row=5, column=5):
-        self.row = row
-        self.column = column
-
-    def get_table_game(self):
-        table = [0] * self.row * self.column
-        return table
+from .logic import SeaBattle
 
 
 def single_player_view(request):
-    sea_battle = SeaBattle(ROW, COLUMN)
+    sea_battle = SeaBattle()
     table = sea_battle.get_table_game()
-    
+
     context = {
-        'table' : table,
-        'row' : ROW,
-        'column' : COLUMN,
-    } 
+        "table": table,
+        "sea_battle": sea_battle,
+    }
 
     return render(request, "sea/single_player.html", context=context)
 
 
-def select(request, butten):
-    return HttpResponse('ok recive selected')
+def select(request, cell):
+    return HttpResponse("ok recive selected")
