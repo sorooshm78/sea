d8796a24affd1e99ce89cde95752f037c88f6879
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-07 11:37:55 +0330

    change name button to cell

--- sea/urls.py
+++ sea/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.single_player_view, name="single_player"),
-    path('<int:button>', views.select, name="select"),
+    path("", views.single_player_view, name="single_player"),
+    path("<int:cell>", views.select, name="select_cell"),
 ]
