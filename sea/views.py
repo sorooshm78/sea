f0c6550a19670dd00154bd0b747dc4ac9669c404
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-12 19:44:52 +0330

    remove unuse import

--- sea/views.py
+++ sea/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
 from .logic import SeaBattle, Cell
