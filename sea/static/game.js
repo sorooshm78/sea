4c1ba1ab677761bd67ad1ac74847c152fa90fa35
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-18 19:06:47 +0330

    add report to game

--- sea/static/game.js
+++ sea/static/game.js
@@ -20,17 +20,24 @@ function select(x, y) {
     $.get(`/select/?x=${x}&y=${y}`, function (data, status) {
         console.log(status);
         if (status === 'success') {
+            // Cells select
             for (index in data.cells) {
                 cell = data.cells[index]
                 $(`#${cell.x}${cell.y}`).removeClass('empty').addClass(cell.class);
             }
 
+            // End Game
             if (data.message) {
                 endGame(data.message);
             } else {
                 enableEmptyButtons();
             }
 
+            // Report Game
+            $('#4-ships').text(data.report['4_ships']);
+            $('#3-ships').text(data.report['3_ships']);
+            $('#2-ships').text(data.report['2_ships']);
+            $('#1-ships').text(data.report['1_ships']);
         }
     });
 }
