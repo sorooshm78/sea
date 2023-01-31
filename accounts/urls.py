9418166281288a5ab6c05c579a5c30368e4d12d7
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-31 23:12:43 +0330

    add new account template

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name="login_user"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout_user"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", views.UserRegistrationView.as_view(), name="register"),
 ]
