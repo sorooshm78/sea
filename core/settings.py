62dfe433d6aa8b1bdb68b5f845b4ae10f126baa8
Soroush mohammadi <sorooshm78@gmail.com>
2023-01-31 19:03:05 +0330

    convert Gregorian date to Persian date

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third party
+    "jalali_date",
     # My app
     "sea",
     "accounts",
@@ -146,4 +147,29 @@ CACHES = {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
     }
+}
+
+# Default settings jalali date
+JALALI_DATE_DEFAULTS = {
+    "Strftime": {
+        "date": "%y/%m/%d",
+        "datetime": "%Y/%m/%d %H:%M:%S",
+    },
+    "Static": {
+        "js": [
+            # loading datepicker
+            "admin/js/django_jalali.min.js",
+            # OR
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
+            # 'admin/js/main.js',
+        ],
+        "css": {
+            "all": [
+                "admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css",
+            ]
+        },
+    },
 }
