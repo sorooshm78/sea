3f35ce019140b47d8708eae31da72e9774edf896
Soroush mohammadi <sorooshm78@gmail.com>
2023-02-01 10:48:21 +0330

    refactor code

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from django.urls import reverse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,7 +136,9 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Login redirect
-LOGIN_REDIRECT_URL = "single_player"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "login"
 
 # Cache
 CACHES = {
