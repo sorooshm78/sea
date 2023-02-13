0df5414b73f7cb9198b3ed26a8abbd2750b69b73
Soroush mohammadi <sorooshm78@gmail.com>
2023-02-13 11:12:45 +0330

    add GameRoomModel

--- app/two_player/consumers.py
+++ app/two_player/consumers.py
@@ -2,52 +2,37 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-from django.core.cache import cache
 
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-    
-        self.user = self.scope['user']
-        self.user_id = self.user.id
 
-        online_users = cache.get_or_set('online_users', set())
-        online_users.add(self.user_id)
-        cache.set('online_users', online_users)
-        
-        print('add', self.user_id)
-        print('online', online_users)
+        self.user = self.scope["user"]
 
         async_to_sync(self.channel_layer.group_add)(
-            str(self.user_id),
+            self.user.username,
             self.channel_name,
         )
 
     def disconnect(self, close_code):
-        online_users = cache.get('online_users')
-        online_users.discard(self.user_id)
-        cache.set('online_users', online_users)
-
-        print('del', self.user_id)
-        print('online', online_users)
 
         async_to_sync(self.channel_layer.group_discard)(
-            str(self.user_id),
+            self.user.username,
             self.channel_name,
         )
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data_json["message"]
 
         # send chat message event to the room
         async_to_sync(self.channel_layer.group_send)(
-            self.user_id,
+            self.user.username,
             {
-                'type': 'chat_message',
-                'message': message,
-            }
+                "type": "chat_message",
+                "message": message,
+            },
         )
 
     def chat_message(self, event):
