c5ab6540227574cff3411bd3404962b7f4f1841d
Soroush mohammadi <sorooshm78@gmail.com>
2023-02-13 16:24:27 +0330

    split to many function

--- app/static/sea_battle/js/two_player.js
+++ app/static/sea_battle/js/two_player.js
@@ -1,9 +1,18 @@
+function showUserInfo(user_info) {
+    $("#user_info").text(user_info);
+}
+
+function receiveData(data) {
+    if (data['user_info']) {
+        showUserInfo(data.user_info);
+    }
+}
+
 $(document).ready(function () {
     gameSocket = new WebSocket("ws://" + window.location.host + "/ws/");
 
     gameSocket.onmessage = function (e) {
         const data = JSON.parse(e.data);
-        user_info = data.user_info;
-        $("#user_info").text(user_info);
+        receiveData(data);
     };
 });
