58442e787c0d792dd1db01cda8b59856aa313d47
Soroush mohammadi <sorooshm78@gmail.com>
2023-02-24 16:57:50 +0330

    add LoginRequiredMixin to home

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "home/index.html"
