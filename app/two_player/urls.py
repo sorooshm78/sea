816efbe458134b0c8827dcef92a7baff35a8e811
Soroush mohammadi <sorooshm78@gmail.com>
2023-03-09 18:57:16 +0330

    bug fix

--- app/two_player/urls.py
+++ app/two_player/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "two_player"
 urlpatterns = [
     path("", views.TwoPlayerView.as_view(), name="two_player"),
-    path("search-user", views.SearchUserView.as_view(), name="search_user"),
-    path("exit-game", views.ExitGameView.as_view(), name="exit_game"),
-    path("new-game", views.NewGameView.as_view(), name="new_game"),
+    path("search-user/", views.SearchUserView.as_view(), name="search_user"),
+    path("exit-game/", views.ExitGameView.as_view(), name="exit_game"),
+    path("new-game/", views.NewGameView.as_view(), name="new_game"),
 ]
