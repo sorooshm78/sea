8137be6bba9b087104647dbb9dccdfbf723e8eee
soroosh <sorooshm78@gmail.com>
2023-04-01 19:21:18 +0330

    add game status filter to admin

--- app/history/admin.py
+++ app/history/admin.py
@@ -17,9 +17,26 @@ class GamePlayersFilter(admin.SimpleListFilter):
             return queryset.filter(Q(player1=self.value()) | Q(player2=self.value()))
 
 
+class GameStatusFilter(admin.SimpleListFilter):
+    title = "Game Status"
+    parameter_name = "game_status"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("complete", "complete game"),
+            ("incomplete", "incomplete game"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "complete":
+            return queryset.filter(win__isnull=False)
+        elif self.value() == "incomplete":
+            return queryset.filter(left__isnull=False)
+
+
 class GameHistoryAdmin(admin.ModelAdmin):
     list_display = ("game_players", "win", "left", "time")
-    list_filter = (GamePlayersFilter, "time")
+    list_filter = (GamePlayersFilter, GameStatusFilter, "time")
 
 
 admin.site.register(GameHistoryModel, GameHistoryAdmin)
