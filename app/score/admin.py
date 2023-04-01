aada601ff167ae391215f992ef6334b8e4798a01
soroosh <sorooshm78@gmail.com>
2023-04-01 16:55:00 +0330

    add model admin to score app

--- app/score/admin.py
+++ app/score/admin.py
@@ -3,4 +3,11 @@ from .models import ScoreBoardModel
 
 
 # Register your models here.
-admin.site.register(ScoreBoardModel)
+class ScoreBoardAdmin(admin.ModelAdmin):
+    list_display = ("user", "score", "time")
+    list_filter = ("user", "time")
+    search_fields = ("user__username__startswith",)
+    ordering = ("-score",)
+
+
+admin.site.register(ScoreBoardModel, ScoreBoardAdmin)
